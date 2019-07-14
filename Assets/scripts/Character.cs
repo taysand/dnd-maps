c5d0cfcb717aab77cb387a5294aba55c309eca38
Taylor Sandusky <tsandusky@mills.edu>
2019-07-13 22:42:16 -0700

    Fix character onMouseDown

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -2,6 +2,11 @@
 using UnityEngine.EventSystems;
 
 public class Character : Draggable {
+
+    public void OnMouseDown() {
+        Debug.Log("click character");
+    }
+
     public override void OnDragDelegate(PointerEventData data) {
         Debug.Log("OnDragDelegate in character");
         base.OnDragDelegate(data);
